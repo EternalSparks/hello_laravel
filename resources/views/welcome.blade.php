c1353e71f2a251aee55cc99135e45759ac1f9f20
杨天任 <yangtrkira@163.com>
2019-02-20 11:40:07 +0800

    区分标识

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    YTR
+                    YTR HELLO LARA
                 </div>
 
                 <div class="links">
